7ed95b49f0e7f521fb151963f9d152826fae8b30
HemanthPranesh <23cb017@kpriet.ac.in>
2026-02-09 09:45:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from recommender import ContentRecommender
 
 if __name__ == "__main__":
-    # Initialize recommender
     rec = ContentRecommender("data.csv")
-
-    # Ask user input
     fav = input("Enter your favorite movie/book: ")
     print("\nTop 5 Recommendations:")
     for r in rec.recommend(fav):
